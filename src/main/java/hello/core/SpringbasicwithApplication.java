248c322035ec9fe5618a1c3534dc11de92bcb901
khaniz <gjf.kee@gmail.com>
2022-08-21 23:16:41 +0900

    Initial commit

---
+++ src/main/java/hello/core/SpringbasicwithApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicwithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbasicwithApplication.class, args);
+    }
+
+}
